60b0efe668e4b0ed36143478b79def80b3278274
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-05 14:08:59 +0530

    api integrated

--- src/components/News.js
+++ src/components/News.js
@@ -2,67 +2,22 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles = [
-    {
-      "source": {
-        "id": "lequipe",
-        "name": "L'equipe"
-      },
-      "author": "L'EQUIPE",
-      "title": "Les Mousquetaires de Paris débutent leur histoire en Ligue européenne",
-      "description": "Pour la première fois en France, une franchise professionnelle de football américain voit le jour. À partir de samedi, les Mousquetaires de Paris vont disputer l'ELF, le championnat européen. Avec des ambitions sportives et populaires.",
-      "url": "https://www.lequipe.fr/Football-americain/Actualites/Les-mousquetaires-de-paris-debutent-leur-histoire-en-ligue-europeenne/1399723",
-      "urlToImage": "https://medias.lequipe.fr/img-photo-jpg/les-mousquetaires-de-paris-sont-prets-a-entrer-en-scene-en-elf-m-le-pihif-dr/1500000001792060/0:0,817:544-640-427-75/8d5e7.jpg",
-      "publishedAt": "2023-06-03T07:00:00+00:00",
-      "content": "C'est une belle année pour les mousquetaires : le chef-d'oeuvre d'Alexandre Dumas a fait son retour sur grand écran et une équipe de football américain est née à Paris. Sauf que là, ils ne seront pas… [+4007 chars]"
-    },
-    {
-      "source": {
-        "id": "talksport",
-        "name": "TalkSport"
-      },
-      "author": "Connor Andrews",
-      "title": "Feyenoord brutally mock crying Jose Mourinho after beating him to Champions League football...",
-      "description": "Feyenoord have posted a brutal takedown of Jose Mourinho, avenging his outburst against them in the quarter-finals. Roma beat the Dutch side on their way to Budapest, where they lost to Sevilla on …",
-      "url": "https://talksport.com/football/1444328/feyenoord-mock-crying-jose-mourinho-champions-league-football/",
-      "urlToImage": "https://talksport.com/wp-content/uploads/sites/5/2023/06/TALKSPORT-MOURINHO-FEYENOORD.png?strip=all&quality=100&w=1500&h=1000&crop=1",
-      "publishedAt": "2023-06-02T16:51:00Z",
-      "content": "Feyenoord have posted a brutal takedown of Jose Mourinho, avenging his outburst against them in the quarter-finals.\r\nRoma beat the Dutch side on their way to Budapest, where they lost to Sevilla on p… [+1792 chars]"
-    },
-    {
-      "source": {
-        "id": "four-four-two",
-        "name": "FourFourTwo"
-      },
-      "author": "Ryan Dabbs, Caio Carrieri",
-      "title": "Fred reveals which Liverpool and Manchester City players he hangs out with in his spare time",
-      "description": "Despite Manchester United's rivalries, Fred finds time for some opposition players outside of football",
-      "url": "https://www.fourfourtwo.com/news/fred-reveals-which-liverpool-and-manchester-city-players-he-hangs-out-with-in-his-spare-time",
-      "urlToImage": "https://cdn.mos.cms.futurecdn.net/MjDDAjN7wXnBETcQM6Mwj5-1200-80.jpg",
-      "publishedAt": "2023-06-01T17:00:06Z",
-      "content": "Fred has revealed he spends time with players from Liverpool and Manchester City away from football, the Manchester United midfielder enjoying the company of the Brazilian contingent situated in the … [+1649 chars]"
-    },
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "Five famous people (and one cat) you didn't know have ESPNcricinfo profiles | ESPNcricinfo.com",
-      "description": "Why do a footballer, a Nobel laureate and a prime minister (no, not Imran Khan) find themselves in the ESPNcricinfo player database? | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29102695/five-famous-people-one-cat-know-espncricinfo-profiles",
-      "urlToImage": "https://a.espncdn.com/i/cricket/cricinfo/1221668_1296x1296.gif",
-      "publishedAt": "2020-04-27T07:20:43Z",
-      "content": "Why do a cat, a footballer, a Nobel laureate and a prime minister find themselves in the ESPNcricinfo database? Here are six player profiles you wouldn't have expected we had.\r\nPeter the catThe only … [+5504 chars]"
-    }
-  ]
+
   constructor() {
     super();
     console.log("Hello i am a constructor from news component");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false
     }
+  }
+
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5bb6f4fb93b64fd58e2abd79fcd2544b";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
   }
 
   render() {
@@ -72,7 +27,7 @@ export class News extends Component {
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url} />
+              <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
           })}
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
       <>
         <div className="my-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl?"https://etvbharatimages.akamaized.net/etvbharat/prod-images/1200-675-18670148-648-18670148-1685854463377.jpg":imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
             </div>
           </div>
         </div>
