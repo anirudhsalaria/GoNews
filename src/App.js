31c7a0da8ebf8180e544fdb39c2ab745ef306827
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-08 14:53:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default class App extends Component {
           <NavBar />
             <Routes>
             <Route exact path="/" element={<News key="general" pageSize={20} country="in" category="general" />}></Route>
+            <Route exact path="/general" element={<News key="genera" pageSize={20} country="in" category="general" />}></Route>
             <Route exact path="/business" element={<News key="business" pageSize={20} country="in" category="business" />}></Route>
             <Route exact path="/entertainment" element={<News key="entertainment" pageSize={20} country="in" category="entertainment" />}></Route>
             <Route exact path="/general" element={<News key="general" pageSize={20} country="in" category="general" />}></Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,7 @@ export class NavBar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" to="/">Home</a>
+                                   <Link className='nav-link' to="/general">Home</Link>
                                 </li>
                              
                                 <li className='nav-item'><Link className='nav-link' to="/business">Business</Link></li>
